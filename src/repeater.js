e48228b7d962f7df0fd41b6586be122534166e7f
Will <crossland.w.a@gmail.com>
2016-08-03 21:54:18 +0100

    Slight refactor to improve readability of repeater

--- src/repeater.js
+++ src/repeater.js
@@ -5,7 +5,7 @@ const EventEmitter = require('events'),
 /*
 Repeatedly calls the passed callback at the specified interval until told to stop
 */
-function Repeater(scheduled_execution, initial_interval) {
+function Repeater(setTimeout, initial_interval) {
     EventEmitter.call(this);
     var repeater = this;
     this._is_scheduling = false;
@@ -19,7 +19,17 @@ function Repeater(scheduled_execution, initial_interval) {
     this.start = function(callback) {
         if (repeater._is_scheduling) return;
         repeater._is_scheduling = true;
-        repeater._call_and_reschedule(callback);
+        callback();
+        this._recursiveSetTimeout(callback);
+    }
+
+    this._recursiveSetTimeout = function(callback) {
+        setTimeout(() => {
+            if (repeater._is_scheduling) {
+                callback();
+                this._recursiveSetTimeout(callback);
+            }
+        }, repeater._interval)
     }
 
     this._call_and_reschedule = function(callback) {
@@ -39,20 +49,24 @@ function Repeater(scheduled_execution, initial_interval) {
 }
 util.inherits(Repeater, EventEmitter);
 
-// Adaptor function used to bind to web Audio API and utilise its audio-rate scheduling
-Repeater.create_scheduled_by_audio_context = function(context, initial_interval) {
-    return new Repeater((callback, interval_ms) => {
+function AudioSetTimeout(context) {
+    this.setTimeout = function(callback, time_ms) {
         let source = context.createBufferSource(),
             now = context.currentTime,
             thousandth = context.sampleRate / 1000,
-            scheduled_at = now + (interval_ms / 1000);
+            scheduled_at = now + (time_ms / 1000) - 0.001;
         // a buffer length of 1 sample doesn't work on IOS, so use 1/1000th of a second
         let buffer = context.createBuffer(1, thousandth, context.sampleRate);
         source.addEventListener('ended', callback);
         source.buffer = buffer;
         source.connect(context.destination);
         source.start(scheduled_at);
-    }, initial_interval);
+    }
+}
+
+// Adaptor function used to bind to web Audio API and utilise its audio-rate scheduling
+Repeater.create_scheduled_by_audio_context = function(context, initial_interval) {
+    return new Repeater(new AudioSetTimeout(context).setTimeout, initial_interval);
 }
 
 module.exports = Repeater;
