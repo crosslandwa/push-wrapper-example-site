905437a73dfea8abceda18797604ecc36cd33b7b
Will <crossland.w.a@gmail.com>
2016-08-15 22:33:04 +0100

    Add test for stopping sequence

--- spec/Sequence_spec.js
+++ spec/Sequence_spec.js
@@ -40,6 +40,27 @@ describe('Sequence', () => {
         }, 300);
     });
 
+    it('fires events until stopped', (done) => {
+        let fired_events = [];
+        sequence.addEvent(50, 'capture', { test: 'hello1' });
+        sequence.addEvent(100, 'capture', { test: 'hello2' });
+        sequence.on('capture', (data) => fired_events.push(data));
+        sequence.loop(150);
+        sequence.start();
+
+        setTimeout(() => {
+            sequence.stop();
+        }, 225);
+
+        setTimeout(() => {
+            expect(fired_events.length).toEqual(3);
+            expect(fired_events[0].test).toEqual('hello1');
+            expect(fired_events[1].test).toEqual('hello2');
+            expect(fired_events[2].test).toEqual('hello1');
+            done();
+        }, 300);
+    });
+
     it('does not schedule events beyond the loop end', (done) => {
         let fired_events = [];
         sequence.addEvent(50, 'capture', { test: 'hello1' });
