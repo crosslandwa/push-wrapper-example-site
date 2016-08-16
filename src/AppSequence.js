0db16e3543736901636cb15faf179f9bd8df093f
Will <crossland.w.a@gmail.com>
2016-08-16 21:52:07 +0100

    Delete commented out hard-coded events

--- src/AppSequence.js
+++ src/AppSequence.js
@@ -21,33 +21,6 @@ module.exports = function(Scheduling, nowMs) {
         running = false,
         reportState = function() { console.log(state); sequence.emit(state); };
 
-    // kicks
-//    sequence.addEvent(0, 'play', {player: 0, velocity: 100});
-//    sequence.addEvent(1000, 'play', {player: 0, velocity: 100});
-//    sequence.addEvent(2000, 'play', {player: 0, velocity: 100});
-//    sequence.addEvent(3000, 'play', {player: 0, velocity: 100});
-//
-//    // snares
-//    sequence.addEvent(1000, 'play', {player: 2, velocity: 50});
-//    sequence.addEvent(3000, 'play', {player: 2, velocity: 50});
-//
-//    // hats
-//    foreach([0, 1000, 2000, 3000], (offset) => {
-//        sequence.addEvent(offset + 0, 'play', {player: 4, velocity: 75});
-//        sequence.addEvent(offset + 250, 'play', {player: 4, velocity: 20});
-//        sequence.addEvent(offset + 500, 'play', {player: 4, velocity: 50});
-//        sequence.addEvent(offset + 750, 'play', {player: 4, velocity: 20});
-//    });
-//    sequence.addEvent(0, 'changePitch', {player: 4, interval: 0});
-//    sequence.addEvent(2000, 'changePitch', {player: 4, interval: -2});
-
-//    sequence.loop(4000);
-
-//    sequence.toggle = function() {
-//        running = !running;
-//        running ? sequence.start() : sequence.stop();
-//    }
-
     sequence.handleRecButton = function() {
         switch (state) {
             case (states.idle):
