4886f263da1231869951b568948406d85bc40a13
Will <crossland.w.a@gmail.com>
2016-08-16 21:55:04 +0100

    Remove let time = assignment, thus removing need for blocks inside switch case statements

--- src/AppSequence.js
+++ src/AppSequence.js
@@ -75,17 +75,11 @@ module.exports = function(Scheduling, nowMs) {
     sequence.addEventNow = function(name, data) {
         switch (state) {
             case (states.recording):
-                {
-                    let time = nowMs() - startTime;
-                    sequence.addEvent(time, name, data);
-                    break;
-                }
+                sequence.addEvent(nowMs() - startTime, name, data);
+                break;
             case (states.overdubbing):
-                {
-                    let time = (nowMs() - startTime) % loopLengthMs;
-                    sequence.addEvent(time, name, data);
-                    break;
-                }
+                sequence.addEvent((nowMs() - startTime) % loopLengthMs, name, data);
+                break;
             case (states.armed):
                 startTime = nowMs();
                 sequence.addEvent(0, name, data);
