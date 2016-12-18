c6195ad5595b244a1192cd45d615123a798a2913
Will <crossland.w.a@gmail.com>
2016-12-18 11:10:47 +0000

    Add algorithm for reducing sample names to 8 chars

---
+++ src/sampleNameShortening.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const shortenToEight = s => s.slice(0, 7)
+
+function sampleNameShortening(filename) {
+  let shortened = filename.split('.')[0]
+
+  while (shortened.length > 8) {
+    shortened = removeFirst(shortened, 'e')
+      || removeFirst(shortened, 'a')
+      || removeFirst(shortened, 'o')
+      || removeFirst(shortened, 'i')
+      || removeFirst(shortened, 'u')
+      || shortenToEight(shortened)
+  }
+  return shortened
+}
+
+function removeFirst(s, char) {
+  if (!s.includes(char)) return
+  let index = s.indexOf(char)
+  return s.slice(0, index) + s.slice(index + 1)
+}
+
+
+
+module.exports = sampleNameShortening
